a653ee78c05667399fc8bc4142ca4f084574ede8
bijonlahiri <bijonlahiri@gmail.com>
2026-04-29 15:24:06 +0530

    dout = 16, num_lstm_layers = 2

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -119,8 +119,8 @@ class ModelTrainer:
                 "batch_size": batch_size,
                 "learning_rate": learning_rate,
                 "d_in": 6,
-                "d_out": 32,
-                "num_lstm_layers": 1,
+                "d_out": 16,
+                "num_lstm_layers": 2,
                 "shortcut": True
             }
 
